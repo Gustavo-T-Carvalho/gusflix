486b807c7d7f1988cd702efe548a826684c7aeb5
Gustavo <carvalho.t.gustavo@gmail.com>
2020-07-31 17:19:58 -0300

    Fim da Aula 2

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import{Link} from 'react-router-dom'
-function CadastroVideo() {
-    return (
-      <PageDefault>
-        <h1>Cadastro de Categoria</h1> 
-        
-        <Link to="/">
-            Ir para Home
-        </Link>
-      </PageDefault>
-  
-    ) 
-      
-  }
 
-  export default CadastroVideo;
+function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+  console.log('nomeDaCategoria',nomeDaCategoria);
+  return (
+    <PageDefault>
+      
+      <h1>Cadastro de Categoria: {nomeDaCategoria} </h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+            value = {nomeDaCategoria}
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
